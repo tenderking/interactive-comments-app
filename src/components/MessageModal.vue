ec13e971a4a846a621889c1a812d893a477c97ef
George Mushore <gmu003@gmail.com>
2022-01-12 23:21:31 +0100

    first commit

---
+++ src/components/MessageModal.vue
@@ -0,0 +1,69 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+const count = ref(0);
+</script>
+<template>
+  <main>
+    <div class="user-info">
+      <img
+        class="avatar"
+        src="../assets/images/avatars/image-amyrobson.png"
+        alt="amy"
+      />
+      <h2 class="name">test 1</h2>
+      <p class="post-date">1 month ago</p>
+      <div class="reply-btn">
+        <img
+          src="../assets/images/icon-reply.svg"
+          alt="reply icon"
+          class="reply-"
+        />
+        <span class="reply-btn-text">Reply</span>
+      </div>
+    </div>
+    <div class="post-container">
+      <p class="post-text">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id quo fugit,
+        maxime sint laudantium labore porro commodi deserunt reiciendis earum.
+      </p>
+    </div>
+    <aside>
+      <button @click="count++">+</button>
+      <div>{{ count }}</div>
+      <button @click="count--">-</button>
+    </aside>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  width: 40em;
+  background-color: azure;
+  display: grid;
+  grid-template-columns: 15% 1fr;
+  grid-template-rows: 1fr 1fr;
+  margin: auto;
+  aside {
+    grid-row: 1 / span 2;
+    grid-column: 1 / span 1;
+    width: 5em;
+    margin-block: auto;
+  }
+  .user-info {
+    grid-row: 1 / span 1;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 1em;
+width: 100%;
+img{ width:2em}
+    .reply-btn {
+        margin-left:auto;
+     
+      img{
+          width: 1em;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/ReplyModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+      <img  class="avatar"
+        src="../assets/images/avatars/image-amyrobson.png"
+        alt="amy" />
+        <textarea name="reply" id="reply-text-area" v-model="message" cols="50" rows="5"></textarea>
+        <button @click="submitMessage">Reply</button>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+const message=ref('')
+const submitMessage= ()=> console.log(message.value)
+</script>
+
+
+
+<style lang="scss" scoped>
+main{
+     width: 40em;
+  background-color: azure;
+  margin-inline:auto;
+  margin-block: 1em;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  gap: 1em;
+  padding:1em;
+  img{
+      width:2em;
+      
+  }
+}
+</style>
