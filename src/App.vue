e22920a975763b22a26dfdee004305559d06300d
tenderking <gmu003@gmail.com>
2022-01-18 14:06:25 +0100

    created Textarea

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import Tag from "./components/atoms/Tag.vue";
+import TextArea from './components/atoms/TextArea.vue'
 </script>
 
 <template>
   <div class="grid">
     <div class="container">
       <h1>Place the component here</h1>
-      <Tag />
+      <!-- <Tag /> -->
+      <text-area />
     </div>
   </div>
 </template>
 
 <style lang="scss">
+@import url(./assets/styles/main.scss);
 .grid {
   display: grid;
   place-content: center;
@@ -20,6 +23,6 @@ import Tag from "./components/atoms/Tag.vue";
 
 .container {
   padding: 1rem;
-  border: 2px dashed dodgerblue;
+  border: 2px dashed --primary-blue-500;
 }
 </style>
---
+++ src/components/atoms/TextArea.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container">
+        <textarea rows="10" cols="5" placeholder="Add comment..."></textarea>
+    </div>
+</template>
