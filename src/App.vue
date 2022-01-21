a9b870bf0f0c5c38164222ffe3c1d6d1138f0ff0
George Mushore <gmu003@gmail.com>
2022-01-21 01:01:47 +0100

    still working on it

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-
-import CommentModal from './components/molecules/CommentModal.vue';
+	import CommentModal from "./components/molecules/CommentModal.vue";
+	import CommentReply from "./components/molecules/CommentReply.vue";
 </script>
 
 <template>
-  <div class="grid">
-    <CommentModal />
-  </div>
+	<div class="grid">
+		<CommentModal />
+		<CommentReply />
+	</div>
 </template>
 
 <style lang="scss">
-.grid {
-  display: grid;
-  place-content: center;
-  min-height: 100vh;
-  background-color: var(--neutral-100);
-}
-
+	.grid {
+		display: grid;
+		place-content: center;
+		min-height: 100vh;
+		background-color: var(--neutral-100);
+	}
 
 	.container {
 		padding: 1rem;
---
+++ src/components/molecules/CommentReply.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="container">
+		<div class="comment-container">
+			<TextArea class="text-area" />
+			<UserProfile class="user-profile" ref="showUserName" />
+			<PrimaryButton class="btn" />
+		</div>
+	</div>
+</template>
+<script setup lang="ts">
+	import PrimaryButton from "../atoms/buttons/normal/PrimaryButton.vue";
+
+	import UserProfile from "../atoms/UserProfile.vue";
+	import TextArea from "../atoms/TextArea.vue";
+</script>
+<style lang="scss">
+	.comment-container {
+		display: grid;
+		grid-template-rows: 2fr 1fr;
+		grid-template-columns: 1fr 1fr;
+		.text-area {
+			grid-column: 1 / span 2;
+		}
+	}
+	@media (min-width: 35em) {
+		.comment-container {
+			display: grid;
+			grid-template-columns: 1fr 1fr 1fr;
+			grid-template-rows: 1fr 1fr;
+			gap: 1em;
+			.user-profile {
+				grid-column: 1 / span 1;
+				grid-row: 1 / span 1;
+			}
+			.text-area {
+				grid-column: 2 / span 1;
+				grid-row: 1 / span 2;
+			}
+			.btn {
+				width: 105px;
+				grid-column: 3 / span 1;
+				grid-row: 1 / span 1;
+			}
+		}
+	}
+</style>
