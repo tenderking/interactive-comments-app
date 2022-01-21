e3b63a6dd73dce544d110cf0bf0b1963789d2ab2
George Mushore <gmu003@gmail.com>
2022-01-21 19:57:29 +0100

    added user name

--- src/components/molecules/CommentView.vue
+++ src/components/molecules/CommentView.vue
@@ -1,103 +1,104 @@
 <script setup lang="ts">
-import UserProfile from '../atoms/UserProfile.vue';
-import LikeButton from '../atoms/buttons/LikeButton.vue';
-import DeleteButton from '../atoms/buttons/flat/DeleteButton.vue';
-import EditButton from '../atoms/buttons/flat/EditButton.vue';
+	import UserProfile from "../atoms/UserProfile.vue";
+	import LikeButton from "../atoms/buttons/LikeButton.vue";
+	import DeleteButton from "../atoms/buttons/flat/DeleteButton.vue";
+	import EditButton from "../atoms/buttons/flat/EditButton.vue";
 </script>
 
 <template>
-  <div class="comment">
-    <div class="comment__user-infos">
-      <UserProfile />
-      <p class="timestamp">1 day ago</p>
-    </div>
-    <div class="comment__content">
-      <p>
-        <span class="replying-to">ramsesmiron</span> I couldn't agree more with this. Everything moves so fast
-        and it always seems like everyone knows the newest library/framework.
-        But the fundamentals are what stay constant.
-      </p>
-    </div>
-    <div class="comment__score">
-      <LikeButton />
-    </div>
-    <div class="comment__actions">
-      <DeleteButton />
-      <EditButton />
-    </div>
-  </div>
+	<div class="comment">
+		<div class="comment__user-infos">
+			<UserProfile :isShown="true" />
+			<p class="timestamp">1 day ago</p>
+		</div>
+		<div class="comment__content">
+			<p>
+				<span class="replying-to">ramsesmiron</span> I couldn't agree more with
+				this. Everything moves so fast and it always seems like everyone knows
+				the newest library/framework. But the fundamentals are what stay
+				constant.
+			</p>
+		</div>
+		<div class="comment__score">
+			<LikeButton />
+		</div>
+		<div class="comment__actions">
+			<DeleteButton />
+			<EditButton />
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
-.comment {
-  background-color: var(--white);
-  padding: 1rem;
-  display: grid;
-  gap: 1rem;
-  border-radius: 0.5rem;
-  grid-template-columns: auto 1fr;
-  grid-template-areas:
-    'user_infos user__infos'
-    'content content'
-    'score actions';
+	.comment {
+		background-color: var(--white);
+		padding: 1rem;
+		display: grid;
+		gap: 1rem;
+		border-radius: 0.5rem;
+		grid-template-columns: auto 1fr;
+		grid-template-areas:
+			"user_infos user__infos"
+			"content content"
+			"score actions";
 
-  @media (min-width: 52rem) {
-    align-items: start;
-    grid-template-areas:
-      'score user_infos actions'
-      'score content content';
-  }
+		@media (min-width: 52rem) {
+			align-items: start;
+			grid-template-areas:
+				"score user_infos actions"
+				"score content content";
+		}
 
-  &__user-infos {
-    grid-area: user_infos;
-    grid-column: 1 / -1;
-    width: 100%;
-    display: grid;
-    align-items: center;
-    grid-auto-flow: column;
-    justify-content: space-between;
+		&__user-infos {
+			grid-area: user_infos;
+			grid-column: 1 / -1;
+			width: 100%;
+			display: grid;
+			align-items: center;
+			grid-auto-flow: column;
+			justify-content: space-between;
 
-    .timestamp {
-      color: var(--neutral-600);
-    }
-  }
+			.timestamp {
+				color: var(--neutral-600);
+			}
+		}
 
-  &__content {
-    grid-area: content;
+		&__content {
+			grid-area: content;
 
-    p {
-      color: var(--neutral-600);
+			p {
+				color: var(--neutral-600);
 
-      .replying-to {
-        color: var(--primary-500);
-        font-weight: 500;
+				.replying-to {
+					color: var(--primary-500);
+					font-weight: 500;
 
-        &::before {
-          content: '@';
-        }
-      }
-    }
-  }
+					&::before {
+						content: "@";
+					}
+				}
+			}
+		}
 
-  &__actions {
-    grid-area: actions;
-    justify-self: end;
-    display: grid;
-    grid-auto-flow: column;
-    align-items: center;
-  }
+		&__actions {
+			grid-area: actions;
+			justify-self: end;
+			display: grid;
+			grid-auto-flow: column;
+			align-items: center;
+		}
 
-  &__score {
-    grid-area: score;
-    justify-self: start;
-  }
+		&__score {
+			grid-area: score;
+			justify-self: start;
+		}
 
-  @media (min-width: 52rem) {
-    &__user-infos {
-      grid-column: initial;
-      justify-content: start;
-      gap: 1rem;
-    }
-  }
-}
+		@media (min-width: 52rem) {
+			&__user-infos {
+				grid-column: initial;
+				justify-content: start;
+				gap: 1rem;
+			}
+		}
+	}
 </style>
