365d3e4bb09b0f7933d9608db50bcc9160549750
George Mushore <gmu003@gmail.com>
2022-01-21 18:21:16 +0100

    setting up pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+import { createPinia } from 'pinia'
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/styles/main.scss";
 
-createApp(App).mount("#app");
+createApp(App)
+    .use(createPinia())
+    .mount("#app");
+
