04626f6d6083810d5e7ee216f3d076f4f8f1ae5b
George Mushore <gmu003@gmail.com>
2022-01-22 13:07:56 +0100

    fixed: like buttons now working with dynamic data

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,46 +1,53 @@
-import { defineStore } from "pinia";
+import { defineStore, acceptHMRUpdate } from "pinia";
 import userData from "../data.json";
+import { ref } from "vue";
 // import {ChatModels, UserOrCurrentUser, } from "../models";
 
 export const useUser = defineStore({
-  id: "user",
+	id: "user",
 
-  state: () => ({
-    likes: 0,
-    showUserPhoto: true,
-    nameOfUser: "juliusom",
-    currentUser: "juliosomo",
-    imgUrl: "/images/avatars/image-amyrobson.png",
-    comments: userData,
-    data: [],
-  }),
-  getters: {
-    checkUser: (state) => {
-      if (state.currentUser === state.nameOfUser) return true;
-    },
-    getData: (state) => {
-      const replies: (object | void)[] = state.comments.comments.replies;
-      if (replies.length > 0) state.data.push(replies);
-    },
-  },
-  actions: {
-    upvote(id: number) {
-      const comment = this.findComment(id) || this.findReply(id);
-
-      if (comment) comment.score++;
-    },
-    downvote(id: number) {
-      const comment = this.findComment(id) || this.findReply(id);
-
-      if (comment) comment.score--;
-    },
-    findComment(id: number) {
-      return this.comments.comments.find((el) => el.id === id);
-    },
-    findReply(id: number) {
-      return this.comments.comments.find((el) => {
-        return el.replies.find((comment) => comment.id === id);
-      });
-    },
-  },
+	state: () => ({
+		showUserPhoto: true,
+		nameOfUser: "juliusom",
+		currentUser: "juliosomo",
+		imgUrl: "/images/avatars/image-amyrobson.png",
+		comments: userData,
+		data: [],
+	}),
+	getters: {
+		checkUser: (state) => {
+			if (state.currentUser === state.nameOfUser) return true;
+		},
+		// getData: (state) => {
+		//   const replies: (object | void)[] = state.comments.comments.replies;
+		//   if (replies.length > 0) state.data.push(replies);
+		// },
+	},
+	actions: {
+		// upvote(id: number) {
+		//   const comment = this.findComment(id) || this.findReply(id);
+		//   if (comment) comment.score++;
+		// },
+		// downvote(id: number) {
+		//   const comment = this.findComment(id) || this.findReply(id);
+		//   if (comment) comment.score--;
+		// },
+		// findComment(id: number) {
+		//   return this.comments.comments.find((el) => el.id === id);
+		// },
+		// findReply(id: number) {
+		//   return this.comments.comments.find((el) => {
+		//     return el.replies.find((comment) => comment.id === id);
+		//   });
+		// },
+		// upvote() {
+		// 	this.likes++;
+		// },
+		// downvote() {
+		// 	this.likes--;
+		// },
+	},
 });
+if (import.meta.hot) {
+	import.meta.hot.accept(acceptHMRUpdate(useUser, import.meta.hot));
+}
